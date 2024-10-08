0d8e02e40174da2e2f0dd5aa70c9e0de294046de
Daniel Gafni <danielgafni16@gmail.com>
2024-10-08 18:08:57 +0300

    :white_check_mark: add tests for PipesContextLoader (#7)

--- tests/test_java_pipes.py
+++ tests/test_java_pipes.py
@@ -5,63 +5,114 @@ from dagster import (
     asset,
     materialize,
 )
+from typing import Dict, Any
+
 from pathlib import Path
 import subprocess
 import pytest
+from pytest_cases import parametrize
+from dagster._core.pipes.utils import (
+    PipesEnvContextInjector,
+    PipesTempFileContextInjector,
+)
+from dagster._core.pipes.client import PipesContextInjector
+import json
+from hypothesis_jsonschema import from_schema
 
-from dagster._core.pipes.utils import PipesEnvContextInjector
+extras_strategy = from_schema({"type": ["object"]})
+
 
 ROOT_DIR = Path(__file__).parent.parent
 
 CLASS_PATH = ROOT_DIR / "build/classes/java/main/pipes/PipesMappingParamsLoader.class"
 
 
-@pytest.fixture(scope="session")
-def built_jar() -> bool:
+@pytest.fixture(scope="session", autouse=True)
+def built_jar():
     subprocess.run(["./gradlew", "build"], check=True)
-    return True
 
 
-def test_java_pipes_temp_file_context_injector(built_jar: bool):
+EXTRAS_LIST = [
+    {
+        "foo": "bar",
+    },
+    {
+        "foo": "bar",
+        "baz": 1,
+    },
+    {
+        "foo": "bar",
+        "baz": 1,
+        "qux": [1, 2, 3],
+    },
+    {
+        "foo": "bar",
+        "baz": 1,
+        "qux": [1, 2, 3],
+        "quux": {"a": 1, "b": 2},
+    },
+    {
+        "foo": "bar",
+        "baz": 1,
+        "qux": [1, 2, 3],
+        "quux": {"a": 1, "b": 2},
+        "corge": None,
+    },
+]
+
+
+# @pytest.fixture
+# def extras(extras_data: Dict[str, Any]):
+#     return extras_data
+
+
+# 20 samples
+# @settings(max_examples=1)
+# @given(extras=extras_strategy)
+@parametrize("extras", EXTRAS_LIST)
+@parametrize(
+    "context_injector", [PipesEnvContextInjector(), PipesTempFileContextInjector()]
+)
+def test_java_pipes(
+    context_injector: PipesContextInjector,
+    extras: Dict[str, Any],
+    tmpdir_factory,
+    capsys,
+):
+    work_dir = tmpdir_factory.mktemp("work_dir")
+
+    extras_path = work_dir / "extras.json"
+
+    with open(str(extras_path), "w") as f:
+        json.dump(extras, f)
+
     @asset
     def java_asset(
         context: AssetExecutionContext, pipes_subprocess_client: PipesSubprocessClient
     ) -> MaterializeResult:
+        job_name = context.dagster_run.job_name
+
         return pipes_subprocess_client.run(
             context=context,
             command=[
                 "java",
                 "-jar",
                 str(ROOT_DIR / "build/libs/dagster-pipes-java-1.0-SNAPSHOT.jar"),
+                "--env",
+                f"--extras={str(extras_path)}",
+                f"--jobName={job_name}",
             ],
-            extras={"input": "Hello, world!"},
+            extras=extras,
         ).get_materialize_result()
 
-    materialize(
-        [java_asset], resources={"pipes_subprocess_client": PipesSubprocessClient()}
-    )
-
-
-def test_java_pipes_env_context_injector(built_jar: bool):
-    @asset
-    def java_asset(
-        context: AssetExecutionContext, pipes_subprocess_client: PipesSubprocessClient
-    ) -> MaterializeResult:
-        return pipes_subprocess_client.run(
-            context=context,
-            command=[
-                "java",
-                "-jar",
-                str(ROOT_DIR / "build/libs/dagster-pipes-java-1.0-SNAPSHOT.jar"),
-            ],
-            extras={"input": "Hello, world!"},
-        ).get_materialize_result()
-
-    materialize(
+    result = materialize(
         [java_asset],
         resources={
             "pipes_subprocess_client": PipesSubprocessClient(
-                context_injector=PipesEnvContextInjector()
+                context_injector=context_injector
             )
         },
+        raise_on_error=False,
     )
+
+    assert result.success
