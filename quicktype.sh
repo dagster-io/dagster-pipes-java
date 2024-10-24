450d100d7dca7036b110e3faf0913dff60d4e4a7
Daniel Gafni <danielgafni16@gmail.com>
2024-10-24 17:38:59 +0200

    add types generated dir

--- quicktype.sh
+++ quicktype.sh
@@ -2,5 +2,5 @@
 
 # Java
 for schema in jsonschema/pipes/*.schema.json; do
-    quicktype -s schema -l java -o src/main/java/generated/$(basename $schema .schema.json).java $schema
+    quicktype -s schema -l java -o src/main/java/types/$(basename $schema .schema.json).java $schema
 done
