70961a378d82e2065a653c31e0fdb2f58faa1761
GingerYouth <parmalat35@live.com>
2024-11-13 18:29:25 +0300

    Fix of copied  Log levels

--- src/main/java/pipes/logger/PipesLogger.java
+++ src/main/java/pipes/logger/PipesLogger.java
@@ -23,7 +23,7 @@ public class PipesLogger {
     }
 
     public void debug(String message) throws DagsterPipesException {
-        log(new PipesLogLevel(PipesLog.INFO.toValue(), 500), message);
+        log(new PipesLogLevel(PipesLog.DEBUG.toValue(), 500), message);
     }
 
     public void info(String message) throws DagsterPipesException {
@@ -31,19 +31,19 @@ public class PipesLogger {
     }
 
     public void warning(String message) throws DagsterPipesException {
-        log(new PipesLogLevel(PipesLog.INFO.toValue(), 900), message);
+        log(new PipesLogLevel(PipesLog.WARNING.toValue(), 900), message);
     }
 
     public void exception(String message) throws DagsterPipesException {
-        log(new PipesLogLevel(PipesLog.INFO.toValue(), 950), message);
+        log(new PipesLogLevel(PipesLog.EXCEPTION.toValue(), 950), message);
     }
 
     public void error(String message) throws DagsterPipesException {
-        log(new PipesLogLevel(PipesLog.INFO.toValue(), 1000), message);
+        log(new PipesLogLevel(PipesLog.ERROR.toValue(), 1000), message);
     }
 
     public void critical(String message) throws DagsterPipesException {
-        log(new PipesLogLevel(PipesLog.INFO.toValue(), 1100), message);
+        log(new PipesLogLevel(PipesLog.CRITICAL.toValue(), 1100), message);
     }
 
     private void log(PipesLogLevel level, String message) throws DagsterPipesException {
